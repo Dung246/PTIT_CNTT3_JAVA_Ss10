6a8ce5e75af57b1c2cc99bb8be606339cb3c5125
Dung246 <dungoo2605@gmail.com>
2026-02-23 14:55:04 +0700

    Update b3.java

--- src/BTVN/b3.java
+++ src/BTVN/b3.java
@@ -1,4 +1,61 @@
 package BTVN;
 
+abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
+
+interface Swimmable {
+    void swim();
+}
+
+interface Flyable {
+    void fly();
+}
+
+class Duck extends Animal implements Swimmable, Flyable {
+
+    public Duck(String name) {
+        super(name);
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(name + " đang bơi dưới nước");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(name + " đang bay trên trời");
+    }
+}
+
+class Fish extends Animal implements Swimmable {
+
+    public Fish(String name) {
+        super(name);
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(name + " đang bơi trong nước");
+    }
+}
+
 public class b3 {
+    public static void main(String[] args) {
+
+        Duck duck = new Duck("Vịt Donald");
+        Fish fish = new Fish("Cá Nemo");
+
+        System.out.println("--- Vịt ---");
+        duck.swim();
+        duck.fly();
+
+        System.out.println("\n--- Cá ---");
+        fish.swim();
+    }
 }
