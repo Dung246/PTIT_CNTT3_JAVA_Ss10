b00177a364b225f9f6230f9473788c28cc447002
Dung246 <dungoo2605@gmail.com>
2026-02-23 14:54:05 +0700

    Update b2.java

--- src/BTVN/b2.java
+++ src/BTVN/b2.java
@@ -1,4 +1,45 @@
 package BTVN;
 
+abstract class Vehicle {
+    protected String brand;
+
+    public Vehicle(String brand) {
+        this.brand = brand;
+    }
+
+    public abstract void move();
+}
+
+class Car extends Vehicle {
+
+    public Car(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(brand + " - Cách di chuyển: Di chuyển bằng động cơ");
+    }
+}
+
+class Bicycle extends Vehicle {
+
+    public Bicycle(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(brand + " - Cách di chuyển: Di chuyển bằng sức người");
+    }
+}
+
 public class b2 {
+    public static void main(String[] args) {
+        Vehicle v1 = new Car("Toyota");
+        Vehicle v2 = new Bicycle("Giant");
+
+        v1.move();
+        v2.move();
+    }
 }
